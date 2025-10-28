bfe987f63b3c45780fc962c0e9487bf28d9ba650
gegeorge25 <wcj4da@virginia.edu>
2025-10-28 10:00:14 -0400

    script

--- practice.py
+++ practice.py
@@ -1,36 +1,53 @@
 #%%
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-# %%
+from scipy.stats import expon
 
+# Parameters for exponential distribution
+lambda_param = 1
+x = np.linspace(0, 5, 500)
 
-def k(u):
-    return np.exp(-u**2)/np.sqrt(2*np.pi)
+# PDF
+pdf = lambda_param * np.exp(-lambda_param * x)
 
-def lcls(x,y,h=None,plot=True):
-    grid=np.sort(np.unique(x))
-       #k=1/np.sqrt(2*np.pi)
-    if h is None:
-        h=1.06 * np.std(x)* len(x) **(-0.2)
+# CDF
+cdf = 1 - np.exp(-lambda_param * x)
 
-    y_hat = []    
-    for z in grid:
-        num=0
-        dem=0
-        for i in range(len(x)):
-            u = (x[i] -z)/h
-            num+=y[i] * k(u)
-            dem+=k(u)
-        y_hat_z=num/dem
-        y_hat.append(y_hat_z)
-        
-    if plot:
-        sns.scatterplot(data=gdf, y='WeightKg', x='WaistCircumferenceCm',alpha=.05)
-        sns.lineplot(x=grid,y=y_hat, color='orange')
+# Plot
+plt.figure(figsize=(8,5))
+plt.plot(x, pdf, label='PDF (f_X(x))', color='blue', linewidth=2)
+plt.plot(x, cdf, label='CDF (F_X(x))', color='red', linewidth=2)
+plt.fill_between(x, 0, pdf, color='blue', alpha=0.2)  # highlight density under PDF
+plt.title('Exponential Distribution: PDF and CDF')
+plt.xlabel('x')
+plt.ylabel('Probability / Cumulative Probability')
+plt.legend()
+plt.grid(True)
+plt.show()
 
-    # return y_hat, grid # Comment out return so output does not print out full list of y hats
     
     #%%
-    
+ 
+
+# Define x values
+x = np.linspace(0, 1, 500)
+
+# PDF of uniform[0,1]
+pdf = np.ones_like(x)
+
+# CDF of uniform[0,1]
+cdf = x
+
+# Plot
+plt.figure(figsize=(8,5))
+plt.plot(x, pdf, label='PDF', color='blue', linewidth=2)
+plt.plot(x, cdf, label='CDF', color='red', linewidth=2)
+plt.fill_between(x, 0, pdf, color='blue', alpha=0.2)  # shade area under PDF
+plt.title('Uniform Distribution [0,1]: PDF and CDF')
+plt.xlabel('x')
+plt.ylabel('Probability / Cumulative Probability')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# %%
